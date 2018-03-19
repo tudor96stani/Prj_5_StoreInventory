b8709d876eff5a251e8449fda40abe07c95f31d8
Tudor Stanila <tudorstanila@gmail.com>
2018-03-19 19:12:10 +0200

    Initial commit

---
+++ src/main/java/controller/StoreController.java
@@ -0,0 +1,45 @@
+package controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import repository.StoreRepository;
+import model.Product;
+
+public class StoreController { 
+	StoreRepository repo =new StoreRepository();
+	public void readProducts(String f){
+		try {
+			repo.readFile(f);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void addProduct(Product p){
+		try {
+
+			repo.addNewProduct(p);
+		} catch (NumberFormatException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch(IOException e){
+			e.printStackTrace();
+		}
+	}
+	public ArrayList<Product> getProductsCategory(String cat){
+		return repo.getProductsCategory(cat);
+	}
+	
+	public ArrayList<Product> stockSituationProduct(String pname){
+		return repo.stockSituationProduct(pname);
+	}
+	public ArrayList<Product> stockSituation(){
+		return repo.stockSituation();
+	}
+}
