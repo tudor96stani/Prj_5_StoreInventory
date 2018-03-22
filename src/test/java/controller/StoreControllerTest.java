84d0dccee065ad3a9bfb7d9dd4944387f39c5400
Tudor Stanila <tudorstanila@gmail.com>
2018-03-22 10:25:50 +0200

    Primul meu test <3333

---
+++ src/test/java/controller/StoreControllerTest.java
@@ -0,0 +1,13 @@
+package controller;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by tudorstanila on 22/03/2018.
+ */
+public class StoreControllerTest extends TestCase {
+    public void testAddProduct() throws Exception {
+        assertTrue(true);
+    }
+
+}
