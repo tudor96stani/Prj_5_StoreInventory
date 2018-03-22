9cdf06b8658b96f68716fd102ecf26834e46f9ac
Tudor Stanila <tudorstanila@gmail.com>
2018-03-22 11:00:21 +0200

    Add product test

--- src/test/java/controller/StoreControllerTest.java
+++ src/test/java/controller/StoreControllerTest.java
@@ -1,13 +1,24 @@
 package controller;
 
 import junit.framework.TestCase;
+import model.Product;
 
 /**
  * Created by tudorstanila on 22/03/2018.
  */
 public class StoreControllerTest extends TestCase {
+
+    private StoreController ctrl;
+
+    public void setUp() throws Exception{
+        super.setUp();
+        ctrl = new StoreController();
+    }
     public void testAddProduct() throws Exception {
-        assertTrue(true);
+
+        assertTrue(ctrl.stockSituation().size()==4);
+        ctrl.addProduct(new Product(100,"Prod","Prod",1));
+        assertTrue(ctrl.stockSituation().size()==5);
     }
 
 }
