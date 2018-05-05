faca176b8254606454a1ee5da3980755bf488843
Tudor Stanila <tudor96stani@gmail.com>
2018-05-05 14:20:21 +0300

    moved tests

--- src/test/java/controller/StoreControllerTest.java
+++ src/test/java/controller/StoreControllerTest.java
@@ -191,4 +191,59 @@ public class StoreControllerTest extends TestCase {
         assertTrue(ctrl.getProductsCategory("telefon").size()==0);
     }
 
+
+    // -- BIG BANG
+
+    public void testIntegrationBigBang() throws Exception {
+        ctrl.addProduct(new Product(100,"a","Prod",1));
+        assertTrue(ctrl.stockSituation().size()==1);
+
+        ctrl.addProduct(new Product(101,"b","aliment",5));
+        ctrl.addProduct(new Product(102,"c","aliment",5));
+        ctrl.addProduct(new Product(103,"d","telefon",2));
+        assertTrue(ctrl.getProductsCategory("aliment").size()==2);
+
+        List<Product> stock =ctrl.stockSituationProduct("a");
+        int q = 0;
+        for(Product p : stock){
+            q+=p.getQuantity();
+        }
+        assertTrue(q==1);
+    }
+
+    // -- TOP DOWN
+    public void testreqA() throws Exception{
+        ctrl.addProduct(new Product(100,"a","Prod",1));
+        assertTrue(ctrl.stockSituation().size()==1);
+    }
+
+
+    public void testreqAB() throws Exception{
+        ctrl.addProduct(new Product(100,"a","Prod",1));
+        assertTrue(ctrl.stockSituation().size()==1);
+
+        ctrl.addProduct(new Product(101,"b","aliment",5));
+        ctrl.addProduct(new Product(102,"c","aliment",5));
+        ctrl.addProduct(new Product(103,"d","telefon",2));
+        assertTrue(ctrl.getProductsCategory("aliment").size()==2);
+
+    }
+
+
+    public void testreqABC() throws Exception {
+        ctrl.addProduct(new Product(100,"a","Prod",1));
+        assertTrue(ctrl.stockSituation().size()==1);
+
+        ctrl.addProduct(new Product(101,"b","aliment",5));
+        ctrl.addProduct(new Product(102,"c","aliment",5));
+        ctrl.addProduct(new Product(103,"d","telefon",2));
+        assertTrue(ctrl.getProductsCategory("aliment").size()==2);
+
+        List<Product> stock =ctrl.stockSituationProduct("a");
+        int q = 0;
+        for(Product p : stock){
+            q+=p.getQuantity();
+        }
+        assertTrue(q==1);
+    }
 }
